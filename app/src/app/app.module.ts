1a443af8218569f60dcb89976798f0c594cdba82
nahum365 <nahum365@me.com>
2018-10-26 23:50:26 -0400

    Beginning SignUp/LogIn

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,29 +3,34 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from "../pages/home/home";
 import { BasePage } from "../pages/base/base";
 import { TaskModalPage } from "../pages/task-modal/task-modal";
+import { SignupPage } from '../pages/signup/signup';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     BasePage,
-    TaskModalPage
+    TaskModalPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
     BasePage,
-    TaskModalPage
+    TaskModalPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
