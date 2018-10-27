e31db5779bb214f075403b99dcb0b4618ccbe2fc
kevinrosales <jrosale1@terpmail.umd.edu>
2018-10-27 02:35:44 -0400

    last

--- backend.py
+++ backend.py
@@ -10,23 +10,25 @@ mydb = sqlite3.connect('CodeForGood.db')
 cursor = mydb.cursor()
 app = Flask(__name__)
 
-# def register():
-# 	returnJSON = {}
-# 	if request.method == 'POST':
-# 		user = request.json
-# 		email = user['email']
-# 		name = user['name']
-# 		school = user['school']
-# 		cursor.execute('SELECT (%s) FROM STUDENTS', username)
+def register():
+	retunJSON = {}
+	if request.method == 'POST':
+		user = request.json
+		email = user['email']
+		name = user['name']
+		school = user['school']
 
-# 		if cursor.fetch():
-# 			return "User already exist :("
-# 		else:
-# 			cursor.execute('INSERT INTO STUDENTS (ID, EMAIL, NAME, SCHOOL, POINTS) VALUES (NULL, (%s), (%s), (%s), '0')', email, name, school)
-# 			retunJSON.update('totalPoints' = 0)
-# 			retunJSON.update('items' = None)
-# 			returnJSON.update('school' = school)
-# 			return returnJSON
+		if cursor.fetch():
+			return "User already exist :("
+		else:
+			cursor.execute('INSERT INTO STUDENTS (ID, EMAIL, NAME, SCHOOL, POINTS) VALUES (NULL, (%s), (%s), (%s), 0', email, name, school)
+			d1 = {'totalPoints': 0}
+			d2 = {'items': None}
+			d3 = {'school': school}
+			retunJSON.update(d1)
+			retunJSON.update(d2)
+			retunJSON.update(d3)
+			return retunJSON
 
 
 
